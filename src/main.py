9cb32cc7f90b9b61e6be3afbff3eba961eac9212
idkup <idkup137@gmail.com>
2024-03-10 13:59:03 -0400

    initial

---
+++ src/main.py
@@ -0,0 +1,7 @@
+import asyncio
+from pathlib import Path
+import requests
+
+DB_IMPORT = "https://import-test.idkup137.workers.dev/api/import"
+MESSAGE_LOG = "testlog.txt"
+IMPORT_LOG = "exportlog.txt"
